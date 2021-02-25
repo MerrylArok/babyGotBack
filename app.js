ee399338c491aaa3ce5cea9f4287b924f11de7d2
Merryl Arokiasamy <merrylarok@gmail.com>
2021-02-24 20:01:23 -0700

    Added more comments, updated to be responsive. Add option to change audio in future

--- app.js
+++ app.js
@@ -1,55 +1,83 @@
-let previousAudio = ""; //keep track of last keydown
+//keep track of last keydown
+let previousAudio = "";
 
+//get HTML target on mouse click, then get the value of the
+//data-type attribute. 
 window.addEventListener("click", function (e) {
-    //get  HTML target on mouse click, then get the value of the
-    //data-type attribute. 
-
     try {
+
+        //clicking element without data-type attribute throws
+        //an undefined error.
         const keyValue = e.target.attributes["data-type"].value;
         doTheThing(keyValue);
-        //clicking any element without data-type attribute throws
-        //an undefined error.
+
     } catch (error) {
+
         console.log("All good. Clicked unintended element:", e);
+
     }
 })
 
+//get the keyboard code on pressing a key
 window.addEventListener("keydown", function (e) {
+
     const keyValue = e.code;
     doTheThing(keyValue);
+
 })
 
+//Function to play and highlight current keydown.
 function doTheThing(keyValue) {
-    const button = document.querySelector(`.key[data-type="${keyValue}"]`);
+
     // select element with .key && the (keyboard/mouse) event
+    const button = document.querySelector(`.key[data-type="${keyValue}"]`);
+
+    // if button without valid keycode is pressed, do nothing
     if (!button) {
-        // if unidentified button played, do nothing
+
         return;
     }
 
+    //Get the corresponding audio element by matching data-type attribute
     const audio = document.querySelector(`audio[data-type="${keyValue}"]`);
 
+    //Next 3 lines cut previous audio plaback immediately and starts current audio
+    //Disabled due to latency (2-4 ms, which was jarring)
+
     // console.time("audioStop()"); 
-    // audioStop();  s                   //disabled due to latency
+    // audioStop();                   
     // console.timeEnd("audioStop()");
 
-    audio.currentTime = 0; //rewind track to start from 00:00:00
+    //Rewind track  to start from 0:00:00 if same key was pressed again
+    audio.currentTime = 0;
+
     audio.play();
+
+    //remove .active class previous highlight
     removeActive();
+
+    //Assign .active and .activeChild classes to focus highlight on current key
     button.classList.add("active");
     button.children[0].classList.add("activeChild");
+
+    //keeps track of last key ==> to assist removeActive
     previousAudio = `${keyValue}`;
+
 }
 
+
+// function interrupts any sound to play keydown asap
 // function audioStop() {
-//     // function interrupts any sound to play keydown asap
-//     document.querySelector(`audio[data-type="${previousAudio}"]`).pause();
+//        document.querySelector(`audio[data-type="${previousAudio}"]`).pause();
 // }
 
+//removes .active and .activeChild classes previous highlight
 function removeActive() {
+
     if (!previousAudio) {
         return;
     }
     document.querySelector(`.key[data-type="${previousAudio}"]`).classList.remove("active");
     document.querySelector(`.key[data-type="${previousAudio}"]`).children[0].classList.remove("activeChild");
+
 } 
